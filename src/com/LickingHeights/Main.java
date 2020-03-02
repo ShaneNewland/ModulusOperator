859952fccada16796f5dbb0ae967113eddcd78fe
Shane <shane.newland.2023@student.lhschools.org>
2020-03-02 13:01:48 -0500

    OddnessMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,24 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// todo even or odd
+    //todo keep number contained
+    //todo get the lost digit
+
+   //we want to find if a number is even or odd
+   int number = 17;
+   System.out.println("Number is even:"+(number % 2 ==0));
+   System.out.println("Number is odd:"+(number % 2 !=0));
+
+   //testOddness
+
+
+
+
+
+
+
+
     }
+
 }
