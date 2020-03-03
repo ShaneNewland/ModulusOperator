7f5a7694073fd84c5c81b2b62a8455b6deb4afbb
Shane <shane.newland.2023@student.lhschools.org>
2020-03-03 13:23:06 -0500

    LastDigitCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,34 +7,15 @@ public class Main {
     //todo keep number contained
     //todo get the lost digit
 
-   //we want to keep a number contained
-   int number = 4;
-   System.out.println((number%5)+1);
-
-   number = 5;
-   System.out.println((number%5)+1);
-
-   number = 6;
-   System.out.println((number%5)+1);
-
-   number = 8;
-   System.out.println((number%5)+1);
-
-   number = 9;
-   System.out.println((number%5)+1);
-
-   number = 10;
-   System.out.println((number%5)+1);
-
-   number = 11;
-   System.out.println((number%5)+1);
-
-   number = 12;
-   System.out.println((number%5)+1);
-
-   number = 13;
-   System.out.println((number%5)+1);
-
+   //we want to
+   int number = 2999;
+   System.out.println("The last digit is: "+number%10);
+   number = number /10;
+   System.out.println("The second to the last digit is: "+number%10);
+   number = number /10;
+   System.out.println("The third to the last digit is: "+number%10);
+   number = number /10;
+   System.out.println("The fourth to the last digit is: "+number%10);
 
 
 
